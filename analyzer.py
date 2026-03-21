9ee85a8b00549b3d21963186cea3c4cb3c11e084
eje019 <emricndayake@gmail.com>
2026-03-21 16:43:14 +0100

    Feat : description

--- analyzer.py
+++ analyzer.py
@@ -2,7 +2,7 @@ import argparse
 
 print("Bonjour je suis l'analyseur de LOGS")
 
-analyseur = argparse.ArgumentParser()
+analyseur = argparse.ArgumentParser(description="Analyse des fichiers de logs")
 analyseur.add_argument("--source")
 args = analyseur.parse_args()
 analyseur.add_argument("--niveau", default="ALL")
