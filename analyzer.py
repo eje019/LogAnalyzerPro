24be3c91eb86b8574a4d1c9a059bab901ea7525d
eje019 <emricndayake@gmail.com>
2026-03-22 18:26:41 +0100

    Feat : Affichage clair du niveau et du dossier a analyser

--- analyzer.py
+++ analyzer.py
@@ -4,13 +4,13 @@ import glob
 print("Bonjour je suis l'analyseur de LOGS")
 
 analyseur = argparse.ArgumentParser(description="Analyse des fichiers de logs")
-analyseur.add_argument("--source")
+analyseur.add_argument("--source", required=True)
 
 analyseur.add_argument("--niveau", default="ALL")
 
 args = analyseur.parse_args()
 
-# print("Le dossier a analyser est :", args.source, " au niveau :" ,args.niveau ,required=True)
+print(f"Le dossier a analyser est :",args.source, "au niveau :" ,args.niveau)
 
-print("Dossier : ", args.source)
-print("Niveau : ", args.niveau)
+# print("Dossier : ", args.source)
+# print("Niveau : ", args.niveau)
